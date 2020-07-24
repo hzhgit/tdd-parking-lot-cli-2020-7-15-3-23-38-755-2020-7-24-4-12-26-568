7a1a71ab800e658d7764cd148c71c69b07dd4aef
zach-he <971499086@qq.com>
2020-07-24 20:23:33 +0800

    [story1][zach]:featrue:add should_return_ticker_when_par_given_car test

---
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -0,0 +1,23 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_ticker_when_par_given_car() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+
+        //when
+        CarTicket carTicket = parkingLot.park();
+
+        //then
+        assertNotNull(carTicket);
+    }
+}
