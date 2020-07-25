d4aaaaec846f5910f190b2566a1438feb2f18bf5
zach-he <971499086@qq.com>
2020-07-25 16:18:08 +0800

    [story2][zach]:feature:add should_return_unrecognized_parking_ticket_when_customer_fetch_car_given_ticket_used test

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -1,5 +1,6 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
 import com.oocl.cultivation.CarTicket;
 import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingLot;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParkingBoyTest {
     @Test
-    void should_return_unrecognized_parking_ticket_when_customer_fetch_car_given_wrong_ticket(){
+    void should_return_unrecognized_parking_ticket_when_customer_fetch_car_given_wrong_ticket() {
         //given
         CarTicket ticket = new CarTicket();
         ParkingLot parkingLot = new ParkingLot();
@@ -22,13 +23,31 @@ public class ParkingBoyTest {
     }
 
     @Test
+    void should_return_unrecognized_parking_ticket_when_customer_fetch_car_given_ticket_used() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        CarTicket ticket = parkingLot.park(car);
+        Car fetchCar = parkingLot.fetch(ticket);
+
+        //when
+        String message = "";
+        Car fetchAgainCar = parkingLot.fetch(ticket);
+        if(fetchAgainCar == null)
+            message = parkingBoy.say(ticket);
+        //then
+        assertEquals("Unrecognized parking ticket.", message);
+    }
+
+    @Test
     void should_return_please_provide_your_parking_ticket_when_customer_fetch_car_given_none() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
 
         //when
-        String message = parkingBoy.say((CarTicket)(null));
+        String message = parkingBoy.say((CarTicket) (null));
 
         //then
         assertEquals("Please provide your parking ticket.", message);
