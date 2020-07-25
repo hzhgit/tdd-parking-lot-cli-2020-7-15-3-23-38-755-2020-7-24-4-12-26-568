03f451f7b35e80fb5306543b2b8be5ed4f94d5e2
zach-he <971499086@qq.com>
2020-07-25 22:58:07 +0800

    [story4][zach]:feature:add should_parking_car_at_pot2_when_parking_a_car_given_pot1_has_6_empty_postion_and_pot2_has_7_postion test

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -31,4 +31,32 @@ public class SmartParkingBoyTest {
         assertEquals(9,actual);
         assertEquals(10,actua2);
     }
+
+    @Test
+    void should_parking_car_at_pot2_when_parking_a_car_given_pot1_has_6_empty_postion_and_pot2_has_7_postion(){
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot pot1 = new ParkingLot();
+        ParkingLot pot2 = new ParkingLot();
+        parkingLots.add(pot1);
+        parkingLots.add(pot2);
+        for (int i = 0; i < 4; i++) {
+            pot1.park(new Car());
+        }
+        for (int i = 0; i < 3; i++) {
+            pot2.park(new Car());
+        }
+
+        Car car = new Car();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy((ArrayList<ParkingLot>)parkingLots);
+        smartParkingBoy.park(car);
+
+        //when
+        int actual = pot1.getEmptyPostion();
+        int actua2 = pot2.getEmptyPostion();
+
+        //then
+        assertEquals(6,actual);
+        assertEquals(6,actua2);
+    }
 }
