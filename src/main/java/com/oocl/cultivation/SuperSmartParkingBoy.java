7a59b7303cb94dd2c5586d1d54679ebaf05a5000
zach-he <971499086@qq.com>
2020-07-27 22:58:42 +0800

    [story4][zach]:use java8 stream instead for loop to find the pot which has most utilization

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,25 +1,25 @@
 package com.oocl.cultivation;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class SuperSmartParkingBoy extends SmartParkingBoy{
+public class SuperSmartParkingBoy extends SmartParkingBoy {
 
     public SuperSmartParkingBoy(ArrayList<ParkingLot> parkingLots) {
         super(parkingLots);
     }
 
     @Override
-    public CarTicket park(Car car){
+    public CarTicket park(Car car) {
         List<ParkingLot> parkingLots = super.getParkingLots();
-        int selectPotIndex = 0;
-        double minUtilization = 1;
-        for (int i = 0; i < parkingLots.size(); i++) {
-            if(parkingLots.get(i).getUtilization() < minUtilization){
-                minUtilization = parkingLots.get(i).getUtilization();
-                selectPotIndex = i;
-            }
-        }
-        return parkingLots.get(selectPotIndex).park(car);
+
+        parkingLots = this.getParkingLots().stream()
+                .filter(lot -> !lot.isParkingLotFull())
+                .sorted(Comparator.comparing(ParkingLot::getUtilization))
+                .collect(Collectors.toList());
+
+        return parkingLots.get(0).park(car);
     }
 }
