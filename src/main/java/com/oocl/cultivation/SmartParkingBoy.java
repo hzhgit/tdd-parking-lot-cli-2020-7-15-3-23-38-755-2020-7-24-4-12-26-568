114b767d25fc5b6cdb9a2d0ec12a5be9005ce9a0
zach-he <971499086@qq.com>
2020-07-27 21:48:01 +0800

    [story4][zach]:refactor:delete comment

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -13,16 +13,6 @@ public class SmartParkingBoy extends ParkingBoy {
     @Override
     public CarTicket park(Car car) {
         List<ParkingLot> parkingLots = super.getParkingLots();
-//        int parkingLotNumber = parkingLots.size();
-//        int maxNumber = 0;
-//        int selectPotIndex = 0;
-//        for (int i = 0; i < parkingLotNumber; i++) {
-//            if (parkingLots.get(i).getEmptyPostion() > maxNumber) {
-//                maxNumber = parkingLots.get(i).getEmptyPostion();
-//                selectPotIndex = i;
-//            }
-//        }
-
 
         parkingLots = this.getParkingLots().stream().filter(lot -> !lot.isParkingLotFull()).sorted((firstLot, secendLot) -> {
             return secendLot.getEmptyPostion() - firstLot.getEmptyPostion();
